0d89be73a69d2c332b1aff42f50b7a564bba22dc
Elijah Romer <romere88@gmail.com>
2021-10-20 14:04:25 -0500

    finished post /workout route and verified it is functioning properly with client code, setting query string parameter for add exercise interface.

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -5,12 +5,6 @@ router.get(`/workouts`, async (req, res) => {
   console.log(`API GET "/workouts" ROUTE SLAPPED`);
   console.log(req.body);
   try {
-
-    // need to:
-    // find the most recent workout, 
-    // aggregate the duration of each exercise object in the workout.exercises array
-    // append the total of all exercise durations as a field to the selected workout, then deliver to client.
-    // do all of this with queries and not JS
     const aggregatedData = await Workout.aggregate(
       [
         {
@@ -23,47 +17,53 @@ router.get(`/workouts`, async (req, res) => {
     ) //sort top down oldest to newest entries based on "day" field
       .sort({ day: 1 })
 
-    console.log(aggregatedData);
+    // console.log(aggregatedData);
     res.status(200).json(aggregatedData);
 
-    // const workoutData = await Workout.find({}).populate('exercises');
-    // console.log("workoutData is as follows:");
-    // console.log(workoutData[0].exercises);
-    // res.status(200).json(workoutData);
-    // query db for all workouts
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+
+
 router.get(`/workouts/range`, async (req, res) => {
   console.log(`API GET "/workouts/range" ROUTE SLAPPED`);
-  console.log(req.body);
   try {
     // const workoutData = await Workout.find({}).populate('exercises');
     // console.log("workoutData is as follows:");
     // console.log(workoutData);
     // res.status(200).json({ workoutData });
     // query db for all workouts
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+
+
 // apparently this just creates an empty entry in the workout model?
-router.post(`/workouts`, (req, res) => {
+router.post(`/workouts`, async (req, res) => {
   console.log(`API POST "/workouts" ROUTE SLAPPED`);
   console.log(req.body)
   try {
-    res.status(200).json({ "msg": "You have yet to write this route" })
+    const newWorkout = await Workout.create({});
+
+    console.log(newWorkout);
+
+    // delivers the id of the newly created empty workout doc to the client, which in turns sets it to the value of the querystring id parameter.
+    res.status(200).json(newWorkout);
 
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
+
 
 // This actually puts data in the workout model.
 router.put(`/workouts/:id`, (req, res) => {
