eb41d0d730e268b9a49cd1265a1c5a779dbb1fbb
Elijah Romer <romere88@gmail.com>
2021-10-20 06:53:00 -0500

    completed basic infrastructure of routes and router, now filling in logic of routes.

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,3 +1,25 @@
 const router = require(`express`).Router();
 
+router.get(`/workouts`, (req, res) => {
+  console.log(`API GET "/workouts" ROUTE SLAPPED`)
+  try {
+    res.status(200).json({ "msg": "You have yet to write this route" })
+    // query db for all workouts
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put(`/workouts/:id`, (req, res) => {
+  console.log(`API PUT "/workouts/${req.params.id}" ROUTE SLAPPED`)
+  try {
+    res.status(200).json({ "msg": "You have yet to write this route" })
+    // query db for specific workout
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -6,9 +6,16 @@ router.get(`/exercise`, (req, res) => {
   res.status(200).sendFile(path.join(__dirname, `../public/exercise.html`));
 });
 
+router.get(`/stats`, (req, res) => {
+  console.log(`HTML GET "/stats" ROUTE SLAPPED`)
+  res.status(200).sendFile(path.join(__dirname, `../public/stats.html`));
+});
+
+
+
 router.get(`/`, (req, res) => {
   console.log(`HTML GET "/" ROUTE SLAPPED`)
-  res.status(200).send(`/index.html`);
+  res.status(200).sendFile(path.join(__dirname, `../public/index.html`));
 });
 
 
