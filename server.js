2a71797ff44627d1026e2a4e43237306bfe6b9c7
Elijah Romer <romere88@gmail.com>
2021-10-20 16:25:23 -0500

    making deployment changes

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const routes = require(`./controllers`)
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
